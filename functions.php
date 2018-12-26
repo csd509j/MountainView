f4ec099ae06aa9292fa1ded2f43e410f99fb9d9b
Josh Armentano <josh@abidewebdesign.com>
2018-12-26 15:56:01 -0800

    First commit.

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php 
+/*
+ * Theme update checker
+ *
+ * @since Mt. View 1.0
+ */
+require WP_CONTENT_DIR . '/plugins/plugin-update-checker-master/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/csd509j/MtView',
+	__FILE__,
+	'MtView'
+);
+
+$myUpdateChecker->setBranch('master'); 
+
+/*
+ * Setup style sheets
+ *
+ * @since Mt. View 1.0
+ */
+function mtview_theme_enqueue_styles() {
+    
+	$parent_style = 'csdschools';
+	$child_theme = wp_get_theme();
+	$parent_theme_version = $child_theme->parent();
+	
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css', '', $parent_theme_version->version );
+	wp_enqueue_style( 'mtview-style',
+	    get_stylesheet_directory_uri() . '/style.css',
+	    array( $parent_style ),
+	    wp_get_theme()->get('Version')
+	);
+
+}
+add_action( 'wp_enqueue_scripts', 'mtview_theme_enqueue_styles' );
